e9b091a89c4fcf135485735126780cb5d9fe093d
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-06-10 12:29:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function windowSize() {
+  const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  return { width, height };
+}
+
+function handleResize() {
+  const windowSize = windowSize();
+	let h1 = documnet.getElementById('h1');
+	let x = windowSize.width;
+	let y = windowSize.height;
+	h1.innerText = `Width: ${x} and Height: ${y}`;
+  // console.log("Window width:", windowSize.width);
+  // console.log("Window height:", windowSize.height);
+}
+
+window.addEventListener("resize", handleResize);
+
+// Initial window size
+console.log("Initial window width:", getWindowSize().width);
+console.log("Initial window height:", getWindowSize().height);
